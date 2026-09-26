b98d5c4111fd1ba437c0bbf3e37ad808401509e4
mani-Darvish <mani.darvish2020@gmail.com>
2026-09-26 14:53:49 +0330

    fix: restore admin role and authentication

--- src/middleware/admin.middleware.js
+++ src/middleware/admin.middleware.js
@@ -1,6 +1,8 @@
 import AppError from "../utils/AppError.js";
 
 const adminMiddleware = (req, res, next) => {
+    // console.log("REQ.USER:", req.user);
+    // console.log("ROLE:", req.user.role);
     if (req.user.role !== "ADMIN") {
         throw new AppError("Admin access required", 403)
     }
